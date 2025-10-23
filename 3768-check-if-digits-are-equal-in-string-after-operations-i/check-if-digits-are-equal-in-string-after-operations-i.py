1de14907284c729c60eaffc922151ff950c7b5a9
Gautham Vijayaraj <35861219+gauthiii@users.noreply.github.com>
2025-10-23 02:03:19 -0400

    Time: 83 ms (52.93%) | Memory: 17.8 MB (71.17%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
@@ -9,7 +9,6 @@ class Solution:
             for i in range(len(s)-1):
                 x+=str((int(s[i])+int(s[i+1]))%10)
             
-            print(x)
             s=x
 
         return s[0]==s[1]
